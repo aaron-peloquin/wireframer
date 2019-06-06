94387f321dacbe7ca8cce31d2202716ba89b78ec
Aaron P <aaron4uk@gmail.com>
2019-06-05 20:41:19 -0500

    added basic sizing columns

--- Component.js
+++ Component.js
@@ -1,16 +1,33 @@
-customElements.define('wireframe-component', class WireframeComponent extends HTMLElement {
+customElements.define('wf-c', class WFC extends HTMLElement {
   /**
    * `connectedCallback` is a custom-element lifecycle callback
    * which fires whenever the element is added to the document
    */
   connectedCallback() {
-    var currentHTML = ''
+    const componentName = this.getAttribute('component') || 'Generic Component'
+    const componentSize = parseInt(this.getAttribute('size') || 12)
+    const width = Math.floor(100 / 12 * componentSize)
+    const spacingUnit = 5
     const root = this.attachShadow({mode: 'open'})
-    console.log('innerHTML:', this.innerHTML)
     const Element = document.createElement('div')
-    Element.innerHTML = `<style>:host {background-color: blue}</style><em>hithere</em><p><slot /></p>`
+    console.log({componentName, Element})
+    Element.innerHTML = `
+    <style>
+      div {
+        display: inline-block;
+        background-color: #CCC;
+        padding: ${spacingUnit}px;
+        margin: ${spacingUnit}px;
+        border: ${spacingUnit}px solid #AAA;
+        width: calc(${width}% - ${spacingUnit*6}px)
+      }
+    </style>
+    <em>${componentName}</em>
+    <slot>No Children</slot>`
     root.appendChild(Element)
-    console.log('shadowRoot:', this.shadowRoot)
-    // this.innerHTML = ``
   }
 })
+
+const styleTag = document.createElement('style').innerHTML = 'body {background-color: orange}'
+
+const head = document.getElementsByTagName('head').appendChild(styleTag)
