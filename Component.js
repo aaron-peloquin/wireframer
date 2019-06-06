937ee67a05832af8f77ac5638781f5afcd9f50bc
Aaron P <aaron4uk@gmail.com>
2019-06-05 21:25:26 -0500

    adding some colors and font size

--- Component.js
+++ Component.js
@@ -7,29 +7,36 @@ customElements.define('wf-c', class WFC extends HTMLElement {
     const componentName = this.getAttribute('component') || 'Generic Component'
     const componentSize = parseInt(this.getAttribute('size') || 12)
     const width = (100 / 12 * componentSize)
-    const spacingUnit = 5
+    const spacingUnit = 4
     const root = this.attachShadow({mode: 'open'})
     const Element = document.createElement('div')
     Element.id = 'root'
-    console.log({componentName, Element})
     Element.innerHTML = `
     <style>
       #root {
         display: inline-block;
-        background-color: #CCC;
+        vertical-align: top;
+        color: #111;
+        background-color: #8A8;
         padding: ${spacingUnit}px;
         margin: ${spacingUnit}px;
-        border: ${spacingUnit}px solid #AAA;
-        width: calc(${width}% - ${(spacingUnit*6)+4}px)
+        border: ${spacingUnit}px solid #DDD;
+        width: calc(${width}% - ${(spacingUnit*6)+4}px);
+      }
+
+      #root:hover {
+        background-color: #A8A;
+        color: #EEE;
+        border-color: #444;
       }
     </style>
     <div><strong>${componentName}</strong></div>
-    <slot><em>0kids</em></slot>`
+    <slot><em>no children</em></slot>`
     root.appendChild(Element)
   }
 })
 
 const styleTag = document.createElement('style')
 styleTag.type = 'text/css';
-styleTag.appendChild(document.createTextNode(`body {background-color: #EEE}`))
+styleTag.appendChild(document.createTextNode(`body {background-color: #EEE; font-size: 18px;}`))
 document.getElementsByTagName("head")[0].appendChild(styleTag)
