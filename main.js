db426416437472ab1133598145d3490a6306aacb
Aaron P <aaron4uk@gmail.com>
2019-06-08 14:50:01 -0500

    added getInfo

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 customElements.define('wf-c', class WFC extends HTMLElement {
+  getInformation() {
+    const {name, size} = this.getProps()
+    const parentName = this.parentNode.getAttribute('component')
+    return {
+      columns: size,
+      name: name,
+      parentName,
+      tag: `<${name} />`,
+    }
+  }
+
+  constructor() {
+    super()
+    this.addEventListener('click', (e) => {
+      if(e.target===this) {
+        const info = this.getInformation()
+        console.log({info})
+      }
+    })
+  }
+
+  getProps() {
+    return {
+      name:this.getAttribute('component') || 'Generic Component',
+      size: parseInt(this.getAttribute('size')) || 12,
+    }
+  }
+
   /**
    * `connectedCallback` is a custom-element lifecycle callback
    * which fires whenever the element is added to the document
    */
   connectedCallback() {
-    const componentName = this.getAttribute('component') || 'Generic Component'
-    const componentSize = parseInt(this.getAttribute('size')) || 12
-    const width = (100 / 12 * componentSize)
+    const {name, size} = this.getProps()
+    const width = (100 / 12 * size)
     const spacingUnit = 4
     const root = this.attachShadow({mode: 'open'})
     const Element = document.createElement('div')
@@ -30,13 +57,13 @@ customElements.define('wf-c', class WFC extends HTMLElement {
         border-color: #444;
       }
     </style>
-    <div><strong>${componentName}</strong></div>
+    <div><strong>${name}</strong></div>
     <slot><em>no children</em></slot>`
     root.appendChild(Element)
   }
 })
 
 const styleTag = document.createElement('style')
-styleTag.type = 'text/css';
+styleTag.type = 'text/css'
 styleTag.appendChild(document.createTextNode(`body {background-color: #EEE; font-size: 18px;}`))
 document.getElementsByTagName("head")[0].appendChild(styleTag)
